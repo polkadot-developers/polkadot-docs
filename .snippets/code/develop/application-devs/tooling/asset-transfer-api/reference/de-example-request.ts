081cf91ca34169b2e8cb16ffd5e42324f4214c24
nhussein11 <nicolashussein14@gmail.com>
2024-10-03 11:47:22 -0300

    fix: adding transactions lifecycle docs

--- .snippets/code/develop/application-devs/tooling/asset-transfer-api/reference/de-example-request.ts
+++ .snippets/code/develop/application-devs/tooling/asset-transfer-api/reference/de-example-request.ts
@@ -1,20 +1,20 @@
 import {
   AssetTransferApi,
   constructApiPromise,
-} from "@substrate/asset-transfer-api";
+} from '@substrate/asset-transfer-api';
 
 async function main() {
   const { api, specName, safeXcmVersion } = await constructApiPromise(
-    "wss://wss.api.moonbeam.network"
+    'wss://wss.api.moonbeam.network',
   );
   const assetsApi = new AssetTransferApi(api, specName, safeXcmVersion);
 
-  const encodedExt = "0x0a03f977814e90da44bfa03b6295a0616a897441acec821a0600";
+  const encodedExt = '0x0a03f977814e90da44bfa03b6295a0616a897441acec821a0600';
 
   try {
-    const decodedExt = assetsApi.decodeExtrinsic(encodedExt, "call");
+    const decodedExt = assetsApi.decodeExtrinsic(encodedExt, 'call');
     console.log(
-      `Decoded tx:\n ${JSON.stringify(JSON.parse(decodedExt), null, 4)}`
+      `Decoded tx:\n ${JSON.stringify(JSON.parse(decodedExt), null, 4)}`,
     );
   } catch (e) {
     console.error(e);

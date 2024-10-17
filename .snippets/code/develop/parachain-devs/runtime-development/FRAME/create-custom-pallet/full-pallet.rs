628490acf9c1b781e0e35ed87ea98a98192650bd
0xLucca <0xlucca.dev@gmail.com>
2024-10-17 16:13:28 -0300

    More improvements

--- .snippets/code/develop/parachain-devs/runtime-development/FRAME/create-custom-pallet/full-pallet.rs
+++ .snippets/code/develop/parachain-devs/runtime-development/FRAME/create-custom-pallet/full-pallet.rs
@@ -12,8 +12,8 @@ pub mod pallet {
     pub trait Config: frame_system::Config {
         /// The overarching runtime event type.
         type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-        // /// A type representing the weights required by the dispatchables of this pallet.
-        // type WeightInfo: WeightInfo;
+        /// A type representing the weights required by the dispatchables of this pallet.
+        type WeightInfo;
     }
 
     #[pallet::event]
--- .snippets/code/develop/parachain-devs/runtime-development/FRAME/create-custom-pallet/pallet-config.rs
+++ .snippets/code/develop/parachain-devs/runtime-development/FRAME/create-custom-pallet/pallet-config.rs
@@ -2,4 +2,6 @@
 pub trait Config: frame_system::Config {
     /// The overarching runtime event type.
     type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+    /// A type representing the weights required by the dispatchables of this pallet.
+    type WeightInfo;
 }

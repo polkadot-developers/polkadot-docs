fca5f489451327df7ffb7a9dbad2c418252c6251
Erin Shaben <eshaben@icloud.com>
2024-11-13 14:48:22 -0500

    Update js/header-scroll.js
    Co-authored-by: Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>

--- js/header-scroll.js
+++ js/header-scroll.js
@@ -1,15 +1,10 @@
 // The purpose of this script is to move the header up out of view while
 // the user is scrolling down a page
 let lastScrollY = window.scrollY;
+const header = document.querySelector('.md-header__inner');
 
 window.addEventListener('scroll', () => {
-  const header = document.querySelector('.md-header__inner');
-  if (window.scrollY > lastScrollY) {
-    // User is scrolling down - hide the header
-    header.classList.add('hidden');
-  } else {
-    // User is scrolling up - show the header
-    header.classList.remove('hidden');
-  }
+  // Toggle header visibility based on scroll direction
+  header.classList.toggle('hidden', window.scrollY > lastScrollY);
   lastScrollY = window.scrollY;
 });

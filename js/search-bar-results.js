ba92da89f36cb1a8a6cf6bbb4f9272b506429c1b
Erin Shaben <eshaben@icloud.com>
2024-11-13 21:05:23 -0500

    remove console.log and run prettier

--- js/search-bar-results.js
+++ js/search-bar-results.js
@@ -3,25 +3,25 @@
 // results dropdown and so that the dropdown only appears once a user has started
 // to type in the input field
 document.addEventListener('DOMContentLoaded', () => {
-    const searchInput = document.querySelector('.md-search__input');
-    const searchOutput = document.querySelector('.md-search__output');
-    const searchResultMeta = document.querySelector('.md-search-result__meta');
+  const searchInput = document.querySelector('.md-search__input');
+  const searchOutput = document.querySelector('.md-search__output');
+  const searchResultMeta = document.querySelector('.md-search-result__meta');
 
-    if (searchResultMeta.textContent.trim() === "Initializing search") {
-        searchResultMeta.style.display = 'none';
-    }
+  if (searchResultMeta.textContent.trim() === 'Initializing search') {
+    searchResultMeta.style.display = 'none';
+  }
 
+  searchInput.addEventListener('input', () => {
     // Only show the search results if the user has started to type
     // Toggle "visible" class based on input content
-    searchInput.addEventListener('input', () => {
-        searchOutput.classList.toggle('visible', searchInput.value.trim() !== '');
-        console.log(searchInput.value);
-        // Do not show the search result meta text unless a user has started typing
-        // a value in the input field
-        if (searchInput.value.trim() === '' && searchResultMeta) {
-            searchResultMeta.style.display = 'none';
-        } else if (searchInput.value.trim().length > 0) {
-            searchResultMeta.style.display = 'block';
-        }
-    });
+    searchOutput.classList.toggle('visible', searchInput.value.trim() !== '');
+
+    // Do not show the search result meta text unless a user has started typing
+    // a value in the input field
+    if (searchInput.value.trim() === '' && searchResultMeta) {
+      searchResultMeta.style.display = 'none';
+    } else if (searchInput.value.trim().length > 0) {
+      searchResultMeta.style.display = 'block';
+    }
+  });
 });

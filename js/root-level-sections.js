f776900e709a977cb7aab40f0f252eaa0063dea0
Erin Shaben <eshaben@icloud.com>
2024-11-25 11:40:51 -0500

    update script to work on gh pages

--- js/root-level-sections.js
+++ js/root-level-sections.js
@@ -5,8 +5,14 @@ document.addEventListener('DOMContentLoaded', function () {
   // Get the current pathname
   const path = window.location.pathname;
 
-  // Check if the path contains exactly one item in its path
   const pathParts = path.split('/').filter((part) => part !== ''); // Split by '/' and remove empty parts
+
+  // Remove 'polkadot-mkdocs' if it's the first part of the path
+  if (pathParts[0] === 'polkadot-mkdocs') {
+    pathParts.shift(); // Remove the first element
+  }
+  
+  // Check if the path contains exactly one item in its path  
   if (pathParts.length === 1) {
     // Select the target element
     const sidebarInner = document.querySelector(

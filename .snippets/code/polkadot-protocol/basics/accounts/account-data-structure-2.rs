e7873a3741a6ad3fd78caa971b6d5886b0d3df25
nhussein11 <nicolashussein14@gmail.com>
2024-12-04 14:01:01 -0300

    fix: account snippets

--- .snippets/code/polkadot-protocol/basics/accounts/account-data-structure-2.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Clone, Eq, PartialEq, Default, RuntimeDebug, Encode, Decode)]
-pub struct AccountInfo<Nonce, AccountData> {
-  pub nonce: Nonce,
-  pub consumers: RefCount,
-  pub providers: RefCount,
-  pub sufficients: RefCount,
-  pub data: AccountData,
-}

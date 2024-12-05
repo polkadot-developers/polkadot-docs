2d00454a99428724bfc9eb407ac4c14441e49f41
0xLucca <0xlucca.dev@gmail.com>
2024-12-05 19:15:23 -0300

    Page ready

--- .snippets/code/tutorials/polkadot-sdk/parachains/build-custom-pallet/build-pallet/scaffold.rs
+++ .snippets/code/tutorials/polkadot-sdk/parachains/build-custom-pallet/build-pallet/scaffold.rs
@@ -4,6 +4,7 @@ pub use pallet::*;
 
 #[frame_support::pallet(dev_mode)]
 pub mod pallet {
+    use super::*;
     use frame_support::pallet_prelude::*;
     use frame_system::pallet_prelude::*;
 
@@ -11,6 +12,5 @@ pub mod pallet {
     pub struct Pallet<T>(_);
 
     #[pallet::config]
-    pub trait Config: frame_system::Config {
-    }
+    pub trait Config: frame_system::Config {}
 }

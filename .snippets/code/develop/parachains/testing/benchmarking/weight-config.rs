ce0795e3aedf003a21b02a502d7206ba473a9f9f
0xLucca <0xlucca.dev@gmail.com>
2025-01-15 16:38:40 -0300

    Update page

--- .snippets/code/develop/parachains/testing/benchmarking/weight-config.rs
+++ .snippets/code/develop/parachains/testing/benchmarking/weight-config.rs
@@ -1,8 +1,10 @@
+pub mod weights;
 use crate::weights::WeightInfo;
 
 /// Configure the pallet by specifying the parameters and types on which it depends.
 #[pallet::config]
 pub trait Config: frame_system::Config {
+    // ...
     /// A type representing the weights required by the dispatchables of this pallet.
     type WeightInfo: WeightInfo;
 }

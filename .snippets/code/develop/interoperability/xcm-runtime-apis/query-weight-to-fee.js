994a770138e549f5e2b50795e0949009d34c65b2
0xLucca <95830307+0xLucca@users.noreply.github.com>
2025-02-04 22:11:48 -0300

    Add XCM Runtime APIs to Develop -> Interoperability (#336)
    * Dry Run API working
    
    * Add xcm payment api
    
    * Update example from Paseo to AssetHub
    
    * wip: xcm payment api
    
    * Xcm payment API ready
    
    * Fix example outputs
    
    * Apply suggestions from code review
    
    Co-authored-by: Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
    
    * Apply suggestions
    
    * Apply suggestions
    
    * Apply suggestions
    
    * Apply suggestions from code review
    
    Co-authored-by: Dawn Kelly <83190195+dawnkelly09@users.noreply.github.com>
    
    * Apply suggestions
    
    ---------
    
    Co-authored-by: Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
    Co-authored-by: Dawn Kelly <83190195+dawnkelly09@users.noreply.github.com>

---
+++ .snippets/code/develop/interoperability/xcm-runtime-apis/query-weight-to-fee.js
@@ -0,0 +1,32 @@
+import { paseoAssetHub } from '@polkadot-api/descriptors';
+import { createClient } from 'polkadot-api';
+import { getWsProvider } from 'polkadot-api/ws-provider/web';
+import { withPolkadotSdkCompat } from 'polkadot-api/polkadot-sdk-compat';
+
+// Connect to the polkadot relay chain
+const client = createClient(
+  withPolkadotSdkCompat(getWsProvider('wss://asset-hub-paseo-rpc.dwellir.com')),
+);
+
+const paseoAssetHubApi = client.getTypedApi(paseoAssetHub);
+
+// Define the weight to convert to fee
+const weight = { ref_time: 15574200000n, proof_size: 359300n };
+
+// Define the versioned asset id
+const versionedAssetId = {
+  type: 'V4',
+  value: { parents: 1, interior: { type: 'Here', value: undefined } },
+};
+
+// Execute the runtime call to convert the weight to fee
+const result =
+  await paseoAssetHubApi.apis.XcmPaymentApi.query_weight_to_asset_fee(
+    weight,
+    versionedAssetId,
+  );
+
+// Print the fee
+console.dir(result.value, { depth: null });
+
+client.destroy();

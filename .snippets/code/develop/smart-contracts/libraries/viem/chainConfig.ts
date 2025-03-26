6d0d717d083e73b22cf0b5a42776e8ae586af097
0xLucca <95830307+0xLucca@users.noreply.github.com>
2025-03-26 12:40:35 -0300

    Apply feedback on viem page (#487)
    * Address feedback
    
    * Apply suggestions from code review
    
    Co-authored-by: Erin Shaben <eshaben@icloud.com>
    
    ---------
    
    Co-authored-by: Erin Shaben <eshaben@icloud.com>

---
+++ .snippets/code/develop/smart-contracts/libraries/viem/chainConfig.ts
@@ -0,0 +1,20 @@
+import { http } from 'viem';
+
+export const TRANSPORT = http('INSERT_RPC_URL');
+
+// Configure the Asset Hub chain
+export const ASSET_HUB = {
+  id: INSERT_CHAIN_ID,
+  name: 'INSERT_CHAIN_NAME',
+  network: 'INSERT_NETWORK_NAME',
+  nativeCurrency: {
+    decimals: INSERT_CHAIN_DECIMALS,
+    name: 'INSERT_CURRENCY_NAME',
+    symbol: 'INSERT_CURRENCY_SYMBOL',
+  },
+  rpcUrls: {
+    default: {
+      http: ['INSERT_RPC_URL'],
+    },
+  },
+} as const;

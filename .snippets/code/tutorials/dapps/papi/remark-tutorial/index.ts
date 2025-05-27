55610f481f1af6417d1c95ed919a2e062dca5419
nhussein11 <nicolashussein14@gmail.com>
2025-05-27 13:14:23 -0300

    fix: formatting and termynal elements fioxed

--- .snippets/code/tutorials/dapps/papi/remark-tutorial/index.ts
+++ .snippets/code/tutorials/dapps/papi/remark-tutorial/index.ts
@@ -1,15 +1,13 @@
 async function withLightClient(): Promise<PolkadotClient> {
-    // Start the light client
-    const smoldot = start();
-    // The Westend Relay Chain
-    const relayChain = await smoldot.addChain({ chainSpec: westEndChainSpec });
-    return createClient(
-        getSmProvider(relayChain)
-    );
+  // Start the light client
+  const smoldot = start();
+  // The Westend Relay Chain
+  const relayChain = await smoldot.addChain({ chainSpec: westEndChainSpec });
+  return createClient(getSmProvider(relayChain));
 }
 
 async function main() {
-    // CLI code goes here...
+  // CLI code goes here...
 }
 
 main();

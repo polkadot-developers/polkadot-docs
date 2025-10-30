0dd0aa6b2ce2c67299b8443edcc1a45b43c321fb
Erin Shaben <eshaben@icloud.com>
2025-10-30 00:21:31 -0400

    delete pages, images, and snippets from the develop section

--- .snippets/code/develop/parachains/maintenance/configure-asynchronous-backing/lib-01.rs
+++
@@ -1,8 +0,0 @@
-// Maximum number of blocks simultaneously accepted by the runtime, not yet included into the
-// relay chain.
-pub const UNINCLUDED_SEGMENT_CAPACITY: u32 = 1;
-// How many parachain blocks are processed by the relay chain per parent. Limits the number of
-// blocks authored per slot.
-pub const BLOCK_PROCESSING_VELOCITY: u32 = 1;
-// Relay chain slot duration, in milliseconds.
-pub const RELAY_chain_SLOT_DURATION_MILLIS: u32 = 6000;
